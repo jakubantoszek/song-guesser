98c8cbfff5cba2a6b02dd3756d595b403f8b26a3
jakubantoszek <102921899+jakubantoszek@users.noreply.github.com>
2026-02-07 12:06:40 +0000

    Add initial classes and static files

---
+++ src/Song.py
@@ -0,0 +1,23 @@
+import os
+from enum import Enum
+
+from config import SONGS_DIR_PATH
+
+class Categories(Enum):
+    ROCK = 0,
+    POP = 1,
+    POP-ROCK = 2,
+    METAL = 3,
+    RAP = 4,
+    POLISH = 10,
+    NON-POLISH = 11
+
+
+class Song:
+    def __init__(self, file_name, categories, level, year):
+        self.artist = file_name.split(" - ")[0]
+        self.title = file_name.split(" - ")[1].split('.')[0]
+        self.path = os.path.join(SONGS_DIR_PATH, file_name)
+        self.categories = categories
+        self.year = year
+        self.level = level
---
+++ src/config.py
@@ -0,0 +1,2 @@
+SONGS_DIR_PATH = "../songs"
+SONGS_DB_PATH = "../static/songs_list.csv"
